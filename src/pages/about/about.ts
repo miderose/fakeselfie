fba1f3c96c5afac017f58ca9cb9aa137abe7acb5
Michele De Rose <michele.derose@nttdata.com>
2018-08-30 15:06:55 +0200

    start

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {Camera, CameraOptions} from "@ionic-native/camera";
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  base64Image: String;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private camera: Camera) {
   }
 
+  takePicture(){
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+
+    this.camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      this.base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      // Handle error
+    })
+  }
 }
